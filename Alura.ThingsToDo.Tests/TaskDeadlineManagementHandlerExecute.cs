372c03a4b49df0de020b42d3e748ca9b6123de4e
hunteradv <gustavopaulody@gmail.com>
2022-02-11 22:18:20 -0300

    Adicionando teste WhenExecuteIsInvokedMustCallRefreshTasksInQuantityTimesOfTotalOfOverdueTasks para verificar quantas vezes o metodo ObtemTarefas é chamado quando cadastra novas tarefas (independente da quantidade).

--- Alura.ThingsToDo.Tests/TaskDeadlineManagementHandlerExecute.cs
+++ Alura.ThingsToDo.Tests/TaskDeadlineManagementHandlerExecute.cs
@@ -3,6 +3,7 @@ using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,6 +49,33 @@ namespace Alura.ThingsToDo.Tests
             var tarefasEmAtraso = repository.ObtemTarefas(t => t.Status == StatusTarefa.EmAtraso);
             Assert.Equal(3, tarefasEmAtraso.Count());
            
+        }
+
+        [Fact]
+        public void WhenExecuteIsInvokedMustCallRefreshTasksInQuantityTimesOfTotalOfOverdueTasks()
+        {
+            //arrange
+            var category = new Categoria("Test");
+
+            var tasks = new List<Tarefa>
+            {
+                new Tarefa(1, "Tirar lixo", category, new DateTime(2022, 02, 07), null, StatusTarefa.Criada),
+                new Tarefa(2, "Fazer o almoço", category, new DateTime(2022, 02, 06), null, StatusTarefa.Criada),
+            };
+
+            var mock = new Mock<IRepositorioTarefas>();
+            mock.Setup(r => r.ObtemTarefas(It.IsAny<Func<Tarefa, bool>>())).Returns(tasks);
+
+            var repository = mock.Object;
+
+            var command = new GerenciaPrazoDasTarefas(new DateTime(2022, 02, 11));
+            var handler = new GerenciaPrazoDasTarefasHandler(repository);
+
+            //act
+            handler.Execute(command);
+
+            //assert
+            mock.Verify(r => r.AtualizarTarefas(It.IsAny<Tarefa[]>()), Times.Once());
         }
     }
 }
